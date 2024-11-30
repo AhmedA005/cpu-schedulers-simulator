31cc1c7ae94ea32753a44ce501ce58b0be4560ba
memo1549 <114335298+memo1549@users.noreply.github.com>
2024-11-30 22:19:41 +0200

    Update PriorityScheduling.java

--- src/Schedulers/PriorityScheduling.java
+++ src/Schedulers/PriorityScheduling.java
@@ -1,21 +1,52 @@
 package Schedulers;
 
-import Processes;
+import Processes.PriorityProcess;
 
+import java.util.Comparator;
 import java.util.List;
 
-public class PriorityScheduling extends Scheduler{
-    public PriorityScheduling(List<Process> ProcessList) {
-        super(ProcessList);
-    }
+/*class MyComparator implements Comparator<Process> {
+    public int compare(Process x, Process y)
+    {
 
-    @Override
+        int comp =  x.getArrivalTime() - y.getArrivalTime();
+        if(comp == 0){
+            comp =  x.getPriority() - y.getPriority();
+        }
+        return comp;
+    }
+}*/
+
+public class PriorityScheduling{
+    private List<PriorityProcess> processList;
+    public PriorityScheduling(List<PriorityProcess> ProcessList) {
+        this.processList = ProcessList;
+    }
+    int time = 0;
+    int size = processList.size();
+    int counter = 0;
+    float avgwaitingTime = 0;
     public void run() {
-
-    }
-
-    @Override
-    protected void calculateAndPrint(List<Process> ProcessList) {
-
+        processList.sort(Comparator.comparingInt(PriorityProcess::getPriority));
+        while(counter < size) {
+            boolean flag = false;
+            for (PriorityProcess p : processList) {
+                if (p.getArrivalTime() <= time) {
+                    flag = true;
+                    p.setTotalWaitingTime(time - p.getArrivalTime());
+                    avgwaitingTime += p.getTotalWaitingTime();
+                    time += p.getBurstTime();
+                    //p.setFinished(true);
+                    System.out.println("Process: " + p.getName());
+                    System.out.println("Waiting time: " + p.getTotalWaitingTime());
+                    processList.remove(p);
+                    counter++;
+                    break;
+                }
+            }
+            if (!flag)
+                time++;
+        }
+        System.out.println("Average waiting time: " + avgwaitingTime/size);
     }
 }
