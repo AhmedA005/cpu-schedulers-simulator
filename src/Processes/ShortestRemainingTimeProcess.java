459bce2eee9bc8c1e2d1e01408bbd7b48f092988
Alaa Ashraf <125699791+alaaashraf19@users.noreply.github.com>
2024-12-04 23:36:33 +0200

    Update ShortestRemainingTimeProcess.java

--- src/Processes/ShortestRemainingTimeProcess.java
+++ src/Processes/ShortestRemainingTimeProcess.java
@@ -5,16 +5,22 @@ public class ShortestRemainingTimeProcess extends Process{
     String state;
     int waitingTime=0;
     int TurnaroundTime=0;
-    int executiontime=0;
-    int agingFactor=1;
+    int agingFactor=10;
     int contextSwitching;
+    int originalBurstTime;
 
-    public ShortestRemainingTimeProcess(String name, int arrivalTime, int burstTime, int priority, String color, String state, int contextSwitching) {
+    public ShortestRemainingTimeProcess(String name, int arrivalTime, int burstTime, int priority, String color, int contextSwitching, String state) {
         super(name, arrivalTime, burstTime, priority, color);
         this.state=state;
         this.contextSwitching=contextSwitching;
-    }
 
+    }
+    public int get_originalBurstTime(){
+        return originalBurstTime;
+    }
+    public void set_originalBurstTime(int new_originalBurstTime){
+        this.originalBurstTime=new_originalBurstTime;
+    }
     public String getState(){ return state;}
 
     public void setState(String newState){ this.state= newState;}
@@ -24,22 +30,20 @@ public class ShortestRemainingTimeProcess extends Process{
     public void incrementWaitingTime(){ this.waitingTime++;}
 
     public int getWaitingTime(){ return waitingTime;}
+    public void set_WaitingTime(int new_WaitingTime){ this.waitingTime= new_WaitingTime;}
 
     public void set_TurnaroundTime(int new_TurnaroundTime){ this.TurnaroundTime= new_TurnaroundTime;}
 
     public int get_TurnaroundTime(){ return TurnaroundTime;}
-
-    public void incrementexecutionTime(){ this.executiontime++;}
-
-    public int getexecutionTime(){ return executiontime;}
-
+    
     public int get_EffectiveBurstTime(int nowTime){
         int waitingTime2=nowTime-this.getArrivalTime();
-        if (waitingTime2 > 2 * this.getBurstTime()) {
+        if (waitingTime2 > 2*this.getBurstTime()) {
             return Math.max(1, this.getBurstTime() - agingFactor * waitingTime2);
         }
         return this.getBurstTime();
     }
     public int getcontextSwitching(){ return contextSwitching;}
-
+    public int getAgingFactor(){ return agingFactor;}
 }
+
