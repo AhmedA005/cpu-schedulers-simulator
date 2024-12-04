c85499aa58a0a18059a29f187678cc19feb6df54
memo1549 <114335298+memo1549@users.noreply.github.com>
2024-12-04 08:19:43 +0200

    Update PriorityProcess.java

--- src/Processes/PriorityProcess.java
+++ src/Processes/PriorityProcess.java
@@ -2,10 +2,13 @@ package Processes;
 
 public class PriorityProcess extends Process {
     private int waitingTime;
+    private int turnaroundTime;
     public PriorityProcess(String name, int arrivalTime, int burstTime,
                            int priority, String color) {
         super(name, arrivalTime, burstTime, priority, color);
     }
     public int getTotalWaitingTime() {return waitingTime;}
     public void setTotalWaitingTime(int waitingTime) {this.waitingTime = waitingTime;}
+    public int getTurnaroundTime() {return turnaroundTime;}
+    public void setTurnaroundTime(int turnaroundTime) {this.turnaroundTime = turnaroundTime;}
 }
