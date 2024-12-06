a3cdf7c39d918cd3ed55e7d0a72c45b022c309c0
memo1549 <114335298+memo1549@users.noreply.github.com>
2024-12-06 02:57:32 +0200

    Update contextSwitchTime

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,16 @@ import Schedulers.PriorityScheduling;
 public class Main {
     public static void main(String[] args) {
         List<Process> FCAIProcesses = new ArrayList<>();
-        FCAIProcesses.add(new PriorityProcess("P1", 0, 17, 4, "red"));
-        FCAIProcesses.add(new PriorityProcess("P2", 3, 6, 9, "blue"));
-        FCAIProcesses.add(new PriorityProcess("P3", 4, 10, 3, "yellow"));
-        FCAIProcesses.add(new PriorityProcess("P4", 29, 4, 10, "white"));
+        FCAIProcesses.add(new PriorityProcess("P1", 0, 4, 0, "red"));
+        FCAIProcesses.add(new PriorityProcess("P2", 1, 8, 1, "blue"));
+        FCAIProcesses.add(new PriorityProcess("P3", 3, 2, 2, "yellow"));
+        FCAIProcesses.add(new PriorityProcess("P4", 10, 6, 3, "white"));
+        FCAIProcesses.add(new PriorityProcess("P5", 12, 5, 4, "white"));
+
+        int contextSwitchTime = 5;
 
         FCAIScheduling scheduler = new FCAIScheduling(FCAIProcesses);
-        PriorityScheduling priorityScheduler = new PriorityScheduling(FCAIProcesses);
+        PriorityScheduling priorityScheduler = new PriorityScheduling(FCAIProcesses, contextSwitchTime);
         priorityScheduler.run();
 //        scheduler.run();
     }
